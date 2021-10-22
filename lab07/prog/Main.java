d1b406c1d3f6547f863ce15356ad2c5173060bc1
Martin (Notebook) <balazsmartin0812@gmail.com>
2021-10-22 11:46:12 +0200

    Update Main.java

--- lab07/prog/Main.java
+++ lab07/prog/Main.java
@@ -14,14 +14,54 @@ public class Main
 
     private static ArrayList<Beer> beers = new ArrayList<Beer>();
     private static HashMap<String, Command> commands = new HashMap<>();
-    private static Map<String, Comparator<Beer>> comparators = new HashMap<>();
-    private static ArrayList<String> params = new ArrayList<String>();
+    private static Map<String, Comparator<Beer>> comps = new HashMap<>();
+    private static List<String> lparams = new LinkedList<>();
 
 
 
+    public static void main(String[] args) throws IOException, ClassNotFoundException
+    {
+        Beer beer1 = new Beer("name1", "style1", 5.5);
+        Beer beer2 = new Beer("name2", "style2", 7.2);
+
+        System.out.println(beer1);
+        System.out.println(beer2);
+
+        commands.put("add", Main::add);
+        commands.put("list", Main::list);
+        commands.put("search", Main::search);
+        commands.put("find", Main::find);
+        commands.put("delete", Main::delete);
+        commands.put("save", Main::save);
+        commands.put("load", Main::load);
+        commands.put("exit", Main::exit);
+
+        comps.put("name", new NameComparator());
+        comps.put("strength", new StrengthComparator());
+        comps.put("style", new StyleComparator());
+
+        lparams.add("name");
+        lparams.add("strength");
+        lparams.add("style");
 
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        while (true)
+        {
+            System.out.print(">>> ");
+            String[] line = br.readLine().split(" ");
+
+
+            if (commands.containsKey(line[0])) {
+                commands.get(line[0]).execute(line);
+            } else {
+                System.out.println("unknown command");
+            }
+
+        }
+
+    }
 
 
 
@@ -36,12 +76,25 @@ public class Main
 
     public static void list(String[] cmd)
     {
-        params.remove(cmd[1]);
-        params.add(0, cmd[1]);
-        Collections.sort(beers, comparators.get(params.get(0)).thenComparing(comparators.get(params.get(1)).thenComparing(comparators.get(params.get(2)))));
+        /*
+        Comparator<Beer> comparator = comps.get("name");
+        if (comparator == null) {
+            Collections.sort(beers, comparator);
+        }
+        */
+
+
+        for (int i=1; i != cmd.length; i += 1) {
+            lparams.remove(cmd[i]);
+            lparams.add(0, cmd[i]);
+        }
+
+
+        Collections.sort(beers, comps.get(lparams.get(0)).thenComparing(comps.get(lparams.get(1)).thenComparing(comps.get(lparams.get(2)))));
         for (Beer beer : beers) {
             System.out.println(beer);
         }
+
     }
 
 
@@ -125,6 +178,7 @@ public class Main
 
 
 
+
     @SuppressWarnings (value="unchecked")
     public static void load(String[] cmd) throws IOException, ClassNotFoundException
     {
@@ -135,56 +189,13 @@ public class Main
     }
 
 
+
+
     public static void exit(String[] cmd)
     {
         System.exit(0);
     }
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException
-    {
-        Beer beer1 = new Beer("name1", "style1", 5.5);
-        Beer beer2 = new Beer("name2", "style2", 7.2);
-
-        System.out.println(beer1);
-        System.out.println(beer2);
-
-        commands.put("add", Main::add);
-        commands.put("exit", Main::exit);
-        commands.put("list", Main::list);
-        commands.put("save", Main::save);
-        commands.put("load", Main::load);
-        commands.put("search", Main::search);
-        commands.put("find", Main::find);
-        commands.put("delete", Main::delete);
-
-        comparators.put("name", new NameComparator());
-        comparators.put("strength", new StrengthComparator());
-        comparators.put("style", new StyleComparator());
-
-        params.add("name");
-        params.add("strength");
-        params.add("style");
-
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        while (true)
-        {
-            System.out.print(">>> ");
-            String[] line = br.readLine().split(" ");
-
-
-            if (commands.containsKey(line[0])) {
-                commands.get(line[0]).execute(line);
-            } else {
-                System.out.println("err");
-            }
-
-        }
-
-        // sc.close();
-
-    }
 
 
 
