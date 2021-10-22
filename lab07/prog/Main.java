f3d5ce40fd34a581130d14aa69866d2481cfb754
Martin (Notebook) <balazsmartin0812@gmail.com>
2021-10-22 11:20:02 +0200

    Update Main.java

--- lab07/prog/Main.java
+++ lab07/prog/Main.java
@@ -1,14 +1,7 @@
 package lab07.prog;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
-import java.util.Iterator;
+import java.io.*;
+import java.util.*;
 
 import lab07.classes.*;
 
@@ -19,93 +12,21 @@ public class Main
 {
 
 
-    private static ArrayList<Beer> beers;
+    private static ArrayList<Beer> beers = new ArrayList<Beer>();
+    private static HashMap<String, Command> commands = new HashMap<>();
+    private static Map<String, Comparator<Beer>> comparators = new HashMap<>();
+    private static ArrayList<String> params = new ArrayList<String>();
 
 
 
 
-    public static void main(String[] args)
-    {
-        Beer beer1 = new Beer("name1", "style1", 5.5);
-        Beer beer2 = new Beer("name2", "style2", 7.2);
-
-        System.out.println(beer1);
-        System.out.println(beer2);
-
-
-        System.out.println("Enter something:");
-        System.out.print(">>> ");
-
-        Scanner sc = new Scanner(System.in);
-        String line = sc.nextLine();
-        String[] spLine = line.split(" ");
-
-        System.out.println("entered: " + spLine[0] + " | length: " + spLine.length + "\n");
-
-
-        beers = new ArrayList<Beer>();
-
-        System.out.println("Available commands: (add, list, save, load)");
-
-        while (true)
-        {
-            System.out.print(">>> ");
-
-            line = sc.nextLine();
-            spLine = line.split(" ");
-
-            if (spLine[0].equals("exit"))
-            {
-                break;
-            }
-
-            else if (spLine[0].equals("add"))
-            {
-                add(spLine);
-            }
-            else if (spLine[0].equals("list"))
-            {
-                list(spLine);
-            }
-            else if (spLine[0].equals("search"))
-            {
-                search(spLine);
-            }
-            else if (spLine[0].equals("find"))
-            {
-                find(spLine);
-            }
-            else if (spLine[0].equals("delete"))
-            {
-                delete(spLine);
-            }
-            else if (spLine[0].equals("save"))
-            {
-                try {
-                    save(spLine);
-                } catch (IOException e) {
-                    System.out.println("Something went wrong:/");
-                }
-            }
-            else if (spLine[0].equals("load"))
-            {
-                try {
-                    load(spLine);
-                } catch (IOException | ClassNotFoundException e) {
-                    System.out.println("Something went wrong:/");
-                }
-            }
-
-        }
-
-        sc.close();
-
-    }
 
 
 
 
-    protected static void add(String[] cmd)
+
+
+    public static void add(String[] cmd)
     {
         beers.add(new Beer(cmd[1], cmd[2], Double.parseDouble(cmd[3])));
     }
@@ -113,35 +34,20 @@ public class Main
 
 
 
-    protected static void list(String[] cmd)
+    public static void list(String[] cmd)
     {
-        if (cmd.length == 2)
-        {
-            if (cmd[1].equals("name"))
-            {
-                Collections.sort(beers, new NameComparator());
-            }
-            else if (cmd[1].equals("style"))
-            {
-                Collections.sort(beers, new StyleComparator());
-            }
-            else if (cmd[1].equals("strength"))
-            {
-                Collections.sort(beers, new StrengthComparator());
-            }
+        params.remove(cmd[1]);
+        params.add(0, cmd[1]);
+        Collections.sort(beers, comparators.get(params.get(0)).thenComparing(comparators.get(params.get(1)).thenComparing(comparators.get(params.get(2)))));
+        for (Beer beer : beers) {
+            System.out.println(beer);
         }
-
-        for (Beer beer : beers)
-        {
-            System.out.println(beer.getName() + " " + beer.getStyle() + " " + beer.getStrength());
-        }
-
     }
 
 
 
 
-    protected static void search(String[] cmd)
+    public static void search(String[] cmd)
     {
         for (Beer beer : beers)
         {
@@ -155,7 +61,7 @@ public class Main
 
 
 
-    protected static void find(String[] cmd)
+    public static void find(String[] cmd)
     {
         for (Beer beer : beers)
         {
@@ -169,7 +75,7 @@ public class Main
 
 
 
-    protected static void delete(String[] cmd)
+    public static void delete(String[] cmd)
     {
         Iterator<Beer> it = beers.iterator();
         while(it.hasNext()){
@@ -207,7 +113,7 @@ public class Main
 
 
 
-    protected static void save(String[] cmd) throws IOException
+    public static void save(String[] cmd) throws IOException
     {
         FileOutputStream fo = new FileOutputStream("./files/" + cmd[1] + ".ser");
         ObjectOutputStream so = new ObjectOutputStream(fo);
@@ -220,7 +126,7 @@ public class Main
 
 
     @SuppressWarnings (value="unchecked")
-    protected static void load(String[] cmd) throws IOException, ClassNotFoundException
+    public static void load(String[] cmd) throws IOException, ClassNotFoundException
     {
         FileInputStream fi = new FileInputStream("./files/" + cmd[1] + ".ser");
         ObjectInputStream oi = new ObjectInputStream(fi);
@@ -228,6 +134,57 @@ public class Main
         oi.close();
     }
 
+
+    public static void exit(String[] cmd)
+    {
+        System.exit(0);
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException
+    {
+        Beer beer1 = new Beer("name1", "style1", 5.5);
+        Beer beer2 = new Beer("name2", "style2", 7.2);
+
+        System.out.println(beer1);
+        System.out.println(beer2);
+
+        commands.put("add", Main::add);
+        commands.put("exit", Main::exit);
+        commands.put("list", Main::list);
+        commands.put("save", Main::save);
+        commands.put("load", Main::load);
+        commands.put("search", Main::search);
+        commands.put("find", Main::find);
+        commands.put("delete", Main::delete);
+
+        comparators.put("name", new NameComparator());
+        comparators.put("strength", new StrengthComparator());
+        comparators.put("style", new StyleComparator());
+
+        params.add("name");
+        params.add("strength");
+        params.add("style");
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true)
+        {
+            System.out.print(">>> ");
+            String[] line = br.readLine().split(" ");
+
+
+            if (commands.containsKey(line[0])) {
+                commands.get(line[0]).execute(line);
+            } else {
+                System.out.println("err");
+            }
+
+        }
+
+        // sc.close();
+
+    }
 
 
 
